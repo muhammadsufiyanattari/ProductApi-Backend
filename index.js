ebc38d74b84d231e5f66d6586b9d9f660ee2fed0
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-12-28 01:23:28 +0500

    add origen

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import express from 'express'
-import { prodcuts } from './ProductData'
+import { prodcuts } from './ProductData.js'
 import cors from "cors";
 const app = express()
 const port = 3000
 
-app.get('/:id', (req, res) => {
+app.use(  cors({origin: ["http://localhost:5173/","http://localhost:3000/","https://sufiyan-ecommerce-website.surge.sh/"]})
+)
+app.get("/",(req,res)=>{
+  res.send("Hello World")
+})
+app.get('/product', (req, res) => {
 //   res.send(`Muhammad Sufiyan ! ${req.params.id}`)
- 
-app.use(cors({origin: ["http://localhost:5173/","http://localhost:3000/","https://sufiyan-ecommerce-website.surge.sh/"]}))
   res.send(
     prodcuts
 )
+})
+app.get("/",(req,res)=>{
+  res.send("Hello World")
 })
 
 app.listen(port, () => {
