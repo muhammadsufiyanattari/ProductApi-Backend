55b90511807202a02fe2ee5b7487043bfa73a90e
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-12-28 00:49:18 +0500

    add cors import in index,js file

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import { prodcuts } from './ProductData'
+import cors from "cors";
 const app = express()
 const port = 3000
 
 app.get('/:id', (req, res) => {
 //   res.send(`Muhammad Sufiyan ! ${req.params.id}`)
+ 
+app.use(cors())
   res.send(
     prodcuts
 )
