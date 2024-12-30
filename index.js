ef3f35ed45642716158857ec676e9dcf5f477384
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-12-30 11:23:03 +0500

    add ecommerce seprate backend

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-import express from 'express'
-import { prodcuts } from './ProductData.js'
+import express from "express";
+import { prodcuts } from "./ProductData.js";
 import cors from "cors";
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(  cors()
-)
-app.get("/",(req,res)=>{
-  res.send("Hello World")
-})
-app.get('/product', (req, res) => {
-//   res.send(`Muhammad Sufiyan ! ${req.params.id}`)
-  res.send(
-    prodcuts
-)
-})
-app.get("/",(req,res)=>{
-  res.send("Hello World")
-})
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/product", (req, res) => {
+  //   res.send(`Muhammad Sufiyan ! ${req.params.id}`)
+  res.send(prodcuts);
+});
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
